75079d06642ae43ba2edc4bb2c2f1d4983ef2cc6
lucky-KRMU <2501730142@krmu.edu.in>
2026-07-18 16:30:53 +0000

    load data in model

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,10 @@
 '''
 This is the python file which will be used to analyze the data.
 '''
+import pandas as pd
+
+data = pd.read_csv('data/housing.csv')
+
+print(type(data))
+print(data.head())
+print(data.info())
