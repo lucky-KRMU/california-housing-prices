929b88169d8f6c1ba23b3a65f6a8635e6ca5bf02
lucky-KRMU <2501730142@krmu.edu.in>
2026-07-19 10:32:58 +0000

    plot histogram graph of total_bedrooms data

--- preprocessing.py
+++ preprocessing.py
@@ -15,11 +15,23 @@ bedroom_mean = data['total_bedrooms'].mean()
 bedroom_median = data['total_bedrooms'].median()
 bedroom_min = data['total_bedrooms'].min()
 bedroom_max = data['total_bedrooms'].max()
+bedroom_mode = data['total_bedrooms'].mode()
 
 print("Mean: ", bedroom_mean)
 print("Median: ", bedroom_median)
+print("Mode: ", bedroom_mode)
 print("Maximum: ", bedroom_max)
 print("Minimum: ", bedroom_min)
 
-plt.plot(data['total_bedrooms'])
-plt.show()
+# Visualising the data through histogram and box plot
+print(type(data['total_bedrooms']))
+# plt.figure(figsize=(8,5))
+# This is for directly using pandas
+# data['total_bedrooms'].hist( bins=20, color='skyblue', edgecolor='black')
+# for explicit matplotlib
+plt.hist(data['total_bedrooms'], bins=100, color='skyblue', edgecolor='black', orientation='horizontal')
+plt.title('Bedroom histogram')
+plt.xlabel('Frequency')
+plt.ylabel('Values')
+
+plt.savefig('graphs/bedrooms_histogram.png')
